f48b093e3ca7b81f4f6ae3fa372afd983fdda156
Anastasia <anastasiaerofeeva121@gmail.com>
2025-11-16 18:00:16 +0300

    config

---
+++ src/config/site.config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Charactify",
+  description: "Create your own hero",
+  navItem: [
+    {href:'/', label:'Треды'},
+    {href:'/add', label:'Добавить'},
+    {href:'/about', label:'О нас'},
+  ]
+}
