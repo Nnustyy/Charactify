7e9c9e2589061fdfce8bc4a492252f7366a29486
Anastasia <anastasiaerofeeva121@gmail.com>
2025-11-20 23:33:23 +0300

    form type

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type FormData = {
+  email:string,
+  password:string,
+  confirmPassword:string
+}
+
+export type FormErrors = {
+  email?:string,
+  password?:string,
+  confirmPassword?:string
+}
