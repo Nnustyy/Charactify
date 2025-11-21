0fc77ca3ea559a948b01b915c1b8623044bededf
Anastasia <anastasiaerofeeva121@gmail.com>
2025-11-21 21:38:56 +0300

    routes upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers/provider";
-import Header from "@/components/UI/header";
+import Header from "@/components/UI/layout/header";
 import { siteConfig } from "@/config/site.config";
 import { layoutConfig } from "@/config/layout.config";
 
